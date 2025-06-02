40173e342e15dfce4a2d7e8deecd57ff4d3abaf5
Radhika Goel <radhikagoel477@gmail.com>
2025-06-02 23:05:40 +0530

    Week 1 task: Hello World React app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Hello World!</h1>
+      <p>Welcome to your React environment </p>
+    </div>
+  );
+}
+
+export default App;
+
